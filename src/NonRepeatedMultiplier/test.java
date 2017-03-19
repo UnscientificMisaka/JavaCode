bc831e9916d23a7c8789bc429a6fe34812502f4b
UnscientificMisaka <2331976499@qq.com>
2017-03-19 20:21:40 +0800

    add reademe

---
+++ src/NonRepeatedMultiplier/test.java
@@ -0,0 +1,30 @@
+package NonRepeatedMultiplier;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class test {
+	public static void main(String[] args){
+		HashSet data = new HashSet(); 
+		List<int[]> list = new ArrayList<int[]>();
+		for(int i = 1;i<10;i++){
+			data.add(i);
+		}
+		
+		Iterator<Integer> j = new
+		
+
+
+//		int arr[] = new int[9];
+//		for(int i = 0;i<9;i++){
+//			int t = (int) (Math.random()*10);			
+//			System.out.println("随机数是"+t);
+//			arr[i] = (int) data.remove(index);
+////			data.remove(index);
+//			System.out.println("第"+i+"个值是:"+arr[i]);
+//		}
+
+		
+	}
+}
